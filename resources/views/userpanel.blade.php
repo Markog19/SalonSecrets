5fca5eedaf2d6012b89a7dcdc0617ad139499397
Markog19 <markog19@gmail.com>
2020-04-09 00:18:16 +0200

    a

--- resources/views/userpanel.blade.php
+++ resources/views/userpanel.blade.php
@@ -383,18 +383,7 @@ window.onclick = function(event) {
 
 </script>
 <script type="text/javascript">
-getCookie("qa_session");
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    console.log(ca);
-}
 
     function fillTable(termini) {
         var i = 0;
